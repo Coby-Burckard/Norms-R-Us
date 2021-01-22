32a207d7484b464d012b59470895a4e5dc5a61b4
Coby-Burckard <51294008+Coby-Burckard@users.noreply.github.com>
2021-01-22 11:14:00 -0600

    probability data

--- app/src/helpers/generateData.ts
+++ app/src/helpers/generateData.ts
@@ -1,14 +1,36 @@
-type data = {    
-  mean?: number;
-  stdv?: number;
+type inputs = {
+  mean: number
+  stdv: number
 }
 
-const generateProbabilityData = (data: data) => {
-  return 1
+const getInputs = (min: number, max: number, points: number): number[] => {
+  const inputs = []
+  const range = max - min
+
+  for (let i = min; i < max; i += range / points) {
+    inputs.push(i)
+  }
+
+  return inputs
 }
 
-const generateCumulativeData = (data: data) => {
-  return 1
+const generateProbabilityData = ({ mean, stdv }: inputs) => {
+  const data = []
+  const min = mean - 4 * stdv
+  const max = mean + 4 * stdv
+
+  const probFunc = (x: number) =>
+    Math.pow(Math.E, ((-1 / 2) * Math.pow(x - mean, 2)) / Math.pow(stdv, 2)) /
+    (stdv * Math.pow(2 * Math.PI, 1 / 2))
+
+  const inputs = getInputs(min, max, 100)
+
+  inputs.forEach((input) => {
+    data.push({
+      input,
+      prob: probFunc(input),
+    })
+  })
 }
 
-export { generateCumulativeData, generateProbabilityData }
+export { generateProbabilityData }
