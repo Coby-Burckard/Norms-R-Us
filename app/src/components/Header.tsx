0c32790c182bf2778d6188fe3fa8187e61369f04
Coby-Burckard <51294008+Coby-Burckard@users.noreply.github.com>
2021-01-24 08:38:02 -0600

    adding github line

--- app/src/components/Header.tsx
+++ app/src/components/Header.tsx
@@ -1,6 +1,13 @@
 import React from "react"
-import { H1 } from "../styles/headerStyles"
+import { H1, Header, ImageLink } from "../styles/headerStyles"
+import github from "./images/github.png"
 
-const Header = () => <H1>Gaussian Distribution</H1>
-
-export default Header
+const Head = () => (
+  <Header>
+    <H1>Gaussian Distribution</H1>
+    <a href="https://github.com/Coby-Burckard/Norms-R-Us">
+      <ImageLink src={github} />
+    </a>
+  </Header>
+)
+export default Head
--- app/src/styles/headerStyles.tsx
+++ app/src/styles/headerStyles.tsx
@@ -1,7 +1,19 @@
 import styled from "styled-components"
 
+const Header = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`
+
 const H1 = styled.h1`
   color: black;
 `
 
-export { H1 }
+const ImageLink = styled.img`
+  height: 50px;
+  width: 50px;
+  margin: 8px 0 0 40px;
+`
+
+export { Header, H1, ImageLink }
